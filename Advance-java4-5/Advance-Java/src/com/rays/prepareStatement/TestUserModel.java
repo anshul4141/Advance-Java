10ea2c27ee417c89b9d0037c4abf5639042b2154
anshul4141 <anshulprajapati691@gmail.com>
2024-08-26 16:59:37 +0530

    done

--- Advance-java4-5/Advance-Java/src/com/rays/prepareStatement/TestUserModel.java
+++ Advance-java4-5/Advance-Java/src/com/rays/prepareStatement/TestUserModel.java
@@ -6,7 +6,63 @@ public class TestUserModel {
 
 	public static void main(String[] args) throws Exception {
 
-		testAdd();
+		// testAdd();
+		// testDelete();
+		// testUpdate();
+		testSearchAll();
+
+	}
+
+	private static void testSearchAll() throws Exception {
+
+		UserModel model = new UserModel();
+
+		UserBean bean = new UserBean();
+
+		bean = model.searchAll();
+
+		if (bean != null) {
+
+			System.out.println(bean.getId());
+			System.out.println(bean.getFirstName());
+			System.out.println(bean.getLastName());
+
+		} else {
+			System.out.println("data not found");
+		}
+
+	}
+
+	private static void testUpdate() throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		UserModel model = new UserModel();
+
+		UserBean bean = new UserBean();
+
+		bean.setId(2);
+		bean.setFirstName("Krishna");
+		bean.setLastName("Yadav");
+		bean.setLoginId("shyam@gmail.com");
+		bean.setPassword("shyam@123");
+		bean.setDob(sdf.parse("2003-03-11"));
+		bean.setPhoneNo("8758632548");
+		bean.setAddress("Bhopal");
+		bean.setGender("Male");
+
+		model.update(bean);
+
+	}
+
+	private static void testDelete() throws Exception {
+
+		UserModel model = new UserModel();
+		UserBean bean = new UserBean();
+
+		bean.setId(2);
+
+		model.delete(bean);
 
 	}
 
--- Advance-java4-5/Advance-Java/src/com/rays/prepareStatement/UserModel.java
+++ Advance-java4-5/Advance-Java/src/com/rays/prepareStatement/UserModel.java
@@ -3,6 +3,7 @@ package com.rays.prepareStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserModel {
 
@@ -27,6 +28,79 @@ public class UserModel {
 		int i = pstmt.executeUpdate();
 
 		System.out.println("data inserted successfully.. " + i);
+
+	}
+
+	public void delete(UserBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/myproject", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("delete from user where id = ?");
+
+		pstmt.setInt(1, bean.getId());
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println("data deleted successfully.. " + i);
+
+	}
+
+	public void update(UserBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/myproject", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement(
+				"update user set firstName = ?, lastName = ?, loginId = ?, password = ?, dob = ?, phoneNo = ?, address = ?, gender = ? where id = ?");
+
+		pstmt.setString(1, bean.getFirstName());
+		pstmt.setString(2, bean.getLastName());
+		pstmt.setString(3, bean.getLoginId());
+		pstmt.setString(4, bean.getPassword());
+		pstmt.setDate(5, new java.sql.Date(bean.getDob().getTime()));
+		pstmt.setString(6, bean.getPhoneNo());
+		pstmt.setString(7, bean.getAddress());
+		pstmt.setString(8, bean.getGender());
+		pstmt.setInt(9, bean.getId());
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println("data update successfully... " + i);
+
+	}
+
+	public UserBean searchAll() throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/myproject", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from user");
+
+		ResultSet rs = pstmt.executeQuery();
+
+		UserBean bean = null;
+
+		while (rs.next()) {
+
+			bean = new UserBean();
+
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setDob(rs.getDate(6));
+			bean.setPhoneNo(rs.getString(7));
+			bean.setAddress(rs.getString(8));
+			bean.setGender(rs.getString(8));
+
+		}
+
+		return bean;
 
 	}
 
