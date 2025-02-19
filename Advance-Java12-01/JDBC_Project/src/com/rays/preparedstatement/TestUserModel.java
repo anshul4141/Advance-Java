8fa82051f016f869f0b74db69d754ec65d1b5e6f
anshul4141 <anshulprajapati691@gmail.com>
2025-02-19 12:43:33 +0530

    done

--- Advance-Java12-01/JDBC_Project/src/com/rays/preparedstatement/TestUserModel.java
+++ Advance-Java12-01/JDBC_Project/src/com/rays/preparedstatement/TestUserModel.java
@@ -1,22 +1,71 @@
 package com.rays.preparedstatement;
 
 import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestUserModel {
 
 	public static void main(String[] args) throws Exception {
 
-		testAdd();
-		//testDelete();
+		// testAdd();
+		// testDelete();
+		// testSearch();
+		testFindByPk();
+
+	}
+
+	private static void testFindByPk() throws Exception {
+
+		UserModel model = new UserModel();
+		UserBean bean = new UserBean();
+
+		bean = model.findByPk(3);
+
+		if (bean == null) {
+			System.out.println("record not found");
+		} else {
+
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getFirstName());
+			System.out.print("\t" + bean.getLastName());
+			System.out.print("\t" + bean.getLoginId());
+			System.out.print("\t" + bean.getPassword());
+			System.out.print("\t" + bean.getAddress());
+			System.out.print("\t" + bean.getGender());
+			System.out.println("\t" + bean.getDob());
+		}
+	}
+
+	private static void testSearch() throws Exception {
+
+		UserModel model = new UserModel();
+		UserBean bean = new UserBean();
+
+		List list = model.search(bean);
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+			bean = (UserBean) it.next();
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getFirstName());
+			System.out.print("\t" + bean.getLastName());
+			System.out.print("\t" + bean.getLoginId());
+			System.out.print("\t" + bean.getPassword());
+			System.out.print("\t" + bean.getAddress());
+			System.out.print("\t" + bean.getGender());
+			System.out.println("\t" + bean.getDob());
+		}
 
 	}
 
 	private static void testDelete() throws Exception {
-		
+
 		UserModel model = new UserModel();
-		
+
 		model.delete(2);
-		
+
 	}
 
 	private static void testAdd() throws Exception {
--- Advance-Java12-01/JDBC_Project/src/com/rays/preparedstatement/UserModel.java
+++ Advance-Java12-01/JDBC_Project/src/com/rays/preparedstatement/UserModel.java
@@ -3,6 +3,9 @@ package com.rays.preparedstatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserModel {
 
@@ -43,6 +46,74 @@ public class UserModel {
 
 		System.out.println("data deleted successfully: " + i);
 
+	}
+
+	public List search(UserBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/myproject", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from user");
+
+		ResultSet rs = pstmt.executeQuery();
+
+		List list = new ArrayList();
+
+		while (rs.next()) {
+			bean = new UserBean();
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setAddress(rs.getString(6));
+			bean.setGender(rs.getString(7));
+			bean.setDob(rs.getDate(8));
+			list.add(bean);
+		}
+
+		return list;
+
+	}
+
+	public UserBean findByPk(int id) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/myproject", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from user where id = ?");
+
+		pstmt.setInt(1, id);
+
+		ResultSet rs = pstmt.executeQuery();
+		UserBean bean = null;
+		while (rs.next()) {
+			bean = new UserBean();
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setAddress(rs.getString(6));
+			bean.setGender(rs.getString(7));
+			bean.setDob(rs.getDate(8));
+
+		}
+
+		return bean;
+
+	}
+
+	// select * from user where loginId = ?
+	public UserBean findByLogin(String loginId) {
+		return null;
+	}
+
+	// select * from user where loginId = ? and password = ?
+	public UserBean authenticate(String loginId, String password) {
+		return null;
 	}
 
 }
