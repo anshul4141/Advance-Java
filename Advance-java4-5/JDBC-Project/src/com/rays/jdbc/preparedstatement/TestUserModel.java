3a3b45b9cf90f3100e4425885bce364d2bd87edc
anshul4141 <anshulprajapati691@gmail.com>
2025-09-18 16:58:26 +0530

    done

--- Advance-java4-5/JDBC-Project/src/com/rays/jdbc/preparedstatement/TestUserModel.java
+++ Advance-java4-5/JDBC-Project/src/com/rays/jdbc/preparedstatement/TestUserModel.java
@@ -1,12 +1,30 @@
 package com.rays.jdbc.preparedstatement;
 
+import java.text.SimpleDateFormat;
+
 public class TestUserModel {
 
 	public static void main(String[] args) throws Exception {
 
+		testAdd();
+
+	}
+
+	public static void testAdd() throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
 		UserModel model = new UserModel();
 
-		System.out.println(model.nextPk());
+		UserBean bean = new UserBean();
+
+		bean.setFirstName("Lucky");
+		bean.setLastName("Tomar");
+		bean.setLogin("lucky@gmail.com");
+		bean.setPassword("lucky123");
+		bean.setDob(sdf.parse("2005-01-18"));
+
+		model.add(bean);
 
 	}
 
--- Advance-java4-5/JDBC-Project/src/com/rays/jdbc/preparedstatement/UserModel.java
+++ Advance-java4-5/JDBC-Project/src/com/rays/jdbc/preparedstatement/UserModel.java
@@ -12,22 +12,43 @@ public class UserModel {
 		int pk = 0;
 
 		Class.forName("com.mysql.cj.jdbc.Driver");
-
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
-
 		PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_user");
-
 		ResultSet rs = pstmt.executeQuery();
 
 		while (rs.next()) {
 			pk = rs.getInt(1);
-			
+			System.out.println("max id: " + pk);
 		}
 
 		conn.close();
-
 		return pk + 1;
 
 	}
+
+	public void add(UserBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("insert into st_user values(?, ?, ?, ?, ?, ?)");
+
+		int pk = nextPk();
+
+		pstmt.setInt(1, pk);
+		pstmt.setString(2, bean.getFirstName());
+		pstmt.setString(3, bean.getLastName());
+		pstmt.setString(4, bean.getLogin());
+		pstmt.setString(5, bean.getPassword());
+		pstmt.setDate(6, new java.sql.Date(bean.getDob().getTime()));
+
+		int i = pstmt.executeUpdate();
+		System.out.println("data inserted successfully: " + i);
+		conn.close();
+	}
+	
+	
+	
 
 }
