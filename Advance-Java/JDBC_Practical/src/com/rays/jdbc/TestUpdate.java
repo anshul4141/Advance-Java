c10de5fc6ead0d719de6a7e82d5aa907da947157
anshul4141 <anshulprajapati691@gmail.com>
2025-12-22 15:57:21 +0530

    done

---
+++ Advance-Java/JDBC_Practical/src/com/rays/jdbc/TestUpdate.java
@@ -0,0 +1,27 @@
+package com.rays.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestUpdate {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+
+		Statement stmt = conn.createStatement();
+
+		int i = stmt.executeUpdate(
+				"update st_user set firstName = 'Birbal', lastName = 'Shekhawat', login = 'birbal@gmail.com', password = 'birbal123' where id = 3");
+
+		System.out.println(i + " row affected..(records updated)");
+
+		conn.close();
+
+	}
+
+}
