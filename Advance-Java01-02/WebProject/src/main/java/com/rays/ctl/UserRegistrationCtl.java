5c06ff1b5bbbceb35524868938642aea8a0c277c
anshul4141 <anshulprajapati691@gmail.com>
2026-02-27 13:58:37 +0530

    done

---
+++ Advance-Java01-02/WebProject/src/main/java/com/rays/ctl/UserRegistrationCtl.java
@@ -0,0 +1,59 @@
+package com.rays.ctl;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.rays.bean.UserBean;
+import com.rays.model.UserModel;
+
+@WebServlet("/UserRegistrationCtl")
+public class UserRegistrationCtl extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher rd = request.getRequestDispatcher("UserRegistrationView.jsp");
+		rd.forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		UserModel model = new UserModel();
+		UserBean bean = new UserBean();
+
+		String fname = request.getParameter("firstName");
+		String lname = request.getParameter("lastName");
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
+		String dob = request.getParameter("dob");
+
+		try {
+			bean.setId(5);
+			bean.setFirstName(fname);
+			bean.setLastName(lname);
+			bean.setLogin(login);
+			bean.setPassword(password);
+			bean.setDob(sdf.parse(dob));
+
+			model.add(bean);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("UserRegistrationView.jsp");
+		rd.forward(request, response);
+
+	}
+
+}
---
+++ Advance-Java01-02/WebProject/src/main/webapp/Header.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h3>Hi, Guest</h3>
+	<a href="LoginCtl">Login</a> |
+	<a href="UserRegistrationCtl">SignUp</a> |
+	<a href="WelcomeCtl">Welcome</a>
+	<hr>
+</body>
+</html>
