8b8044d9996616794e15b3d906d3a20cba0af81c
anshul4141 <anshulprajapati691@gmail.com>
2026-03-03 13:54:02 +0530

    done

--- Advance-Java01-02/WebProject/src/main/java/com/rays/ctl/UserListCtl.java
+++ Advance-Java01-02/WebProject/src/main/java/com/rays/ctl/UserListCtl.java
@@ -1,6 +1,7 @@
 package com.rays.ctl;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,11 +10,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.rays.bean.UserBean;
+import com.rays.model.UserModel;
+
 @WebServlet("/UserListCtl")
 public class UserListCtl extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		List<UserBean> list = null;
+		UserModel model = new UserModel();
+
+		try {
+			list = model.search(null);
+			request.setAttribute("list", list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		RequestDispatcher rd = request.getRequestDispatcher("UserListView.jsp");
 		rd.forward(request, response);
 	}
